51ad6b08849112ec1cd6af0b8b5617deea716394
Jakub Nowosad <tupiszakaczki@gmail.com>
2021-07-10 10:46:23 +0200

    moves reg_isolation to fun

--- R/reg_isolation.R
+++ R/reg_isolation.R
@@ -1,45 +1,57 @@
-library(terra)
-library(sf)
-library(philentropy)
-volcano = rast(system.file("raster/volcano.tif", package = "supercells"))
-vr = read_sf(system.file("regions/volcano_regions.gpkg", package = "laland"))
-
-# version 1 ---------------------------------------------------------------
-n = 20
-dist_fun = "euclidean"
-v = vect(vr)
-iso = vector(mode = "numeric", length = length(ids))
-for (i in seq_len(nrow(vr))){
-  sum_dist = 0
-  n_elem = 0
-  neigh_id = which(relate(vect(vr), vect(vr)[i], relation = "touches"))
-  for (j in neigh_id){
-    vals_i = as.matrix(terra::extract(volcano, v[i])[-1])
-    vals_j = as.matrix(terra::extract(volcano, v[j])[-1])
-    if (length(vals_i) > n){
-      vals_i = vals_i[sample(nrow(vals_i), size = n), , drop = FALSE]
-    }
-    if (length(vals_j) > n){
-      vals_j = vals_j[sample(nrow(vals_j), size = n), , drop = FALSE]
-    }
-    for (vi in seq_len(nrow(vals_i))){
-      for (vj in seq_len(nrow(vals_j))){
-        pair_of_vals = rbind(vals_i[vi, ], vals_j[vj, ])
-        tmp_dist = philentropy::distance(pair_of_vals,
-                                         method = dist_fun,
-                                         mute.message = TRUE)
-        sum_dist = sum_dist + tmp_dist
-        n_elem = n_elem + 1
+#' Title
+#'
+#' @param region
+#' @param raster
+#' @param dist_fun
+#' @param sample_size
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' library(terra)
+#' library(sf)
+#' volcano = rast(system.file("raster/volcano.tif", package = "supercells"))
+#' vr = read_sf(system.file("regions/volcano_regions.gpkg", package = "laland"))
+#' reg_inh = reg_isolation(vr, volcano)
+#'
+#' mean(reg_inh$iso)
+#'
+#' plot(volcano)
+#' plot(vect(vr), add = TRUE)
+#' plot(volcano)
+#' plot(reg_inh["iso"], add = TRUE)
+reg_isolation = function(region, raster, dist_fun = "euclidean", sample_size = 20) {
+  v = vect(region)
+  iso = vector(mode = "numeric", length = length(v))
+  for (i in seq_len(length(v))){
+    sum_dist = 0
+    n_elem = 0
+    neigh_id = which(relate(v, v[i], relation = "touches"))
+    for (j in neigh_id){
+      vals_i = as.matrix(terra::extract(raster, v[i])[-1])
+      vals_j = as.matrix(terra::extract(raster, v[j])[-1])
+      if (length(vals_i) > sample_size){
+        vals_i = vals_i[sample(nrow(vals_i), size = sample_size), , drop = FALSE]
+      }
+      if (length(vals_j) > sample_size){
+        vals_j = vals_j[sample(nrow(vals_j), size = sample_size), , drop = FALSE]
+      }
+      for (vi in seq_len(nrow(vals_i))){
+        for (vj in seq_len(nrow(vals_j))){
+          pair_of_vals = rbind(vals_i[vi, ], vals_j[vj, ])
+          tmp_dist = philentropy::distance(pair_of_vals,
+                                           method = dist_fun,
+                                           # as.dist.obj = TRUE,
+                                           mute.message = TRUE)
+          sum_dist = sum_dist + tmp_dist
+          n_elem = n_elem + 1
+        }
       }
     }
+    iso[i] = sum_dist / n_elem
   }
-  iso[i] = sum_dist / n_elem
+  region$iso = iso
+  return(region)
 }
 
-vr$iso = iso
-mean(iso)
-
-plot(volcano)
-plot(vect(vr), add = TRUE)
-plot(volcano)
-plot(vr["iso"], add = TRUE)
